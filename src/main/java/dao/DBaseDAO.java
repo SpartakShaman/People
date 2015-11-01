b6d2da89d5c8c5ab946147cb675a83e1a57ad74c
Vlad <Vlad@dom2>
2015-11-01 22:32:30 +0200

    add database load

--- src/main/java/dao/DBaseDAO.java
+++ src/main/java/dao/DBaseDAO.java
@@ -5,6 +5,17 @@
  */
 package dao;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.People;
 
 /**
@@ -12,10 +23,44 @@ import model.People;
  * @author Vlad
  */
 public class DBaseDAO extends AbstractDAO{
+    
+    private Connection con = null;
+    private PreparedStatement preparedStatement = null; 
+    
+    private Connection getConnection() throws SQLException, IOException{
+
+            InputStream input = new FileInputStream("jdbc.properties");
+            Properties prop = new Properties();
+            prop.load(input);
+           return DriverManager.getConnection(prop.getProperty("database"), prop.getProperty("dbuser"),
+					prop.getProperty("dbpassword"));
+    }
 
     @Override
     public People load(People people) {
-        System.out.println(this.getClass().getName() + " Load = "+people);
+        String selectSQL = "SELECT namee, surname, birthday FROM people WHERE surname = ?";
+        try {
+            con = getConnection();
+
+            preparedStatement = con.prepareStatement(selectSQL);
+            preparedStatement.setString(1, people.getSurname());
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                String username = rs.getString("namee");
+                String surname = rs.getString("surname");
+                String birthday = rs.getString("birthday");
+                System.out.println(username + " " + surname + " " + birthday);
+            } 
+            preparedStatement.close();
+            con.close();
+        } catch (Exception ex) {
+            Logger.getLogger(DBaseDAO.class.getName()).log(Level.SEVERE, null, ex);
+
+        } finally {
+            preparedStatement = null;
+            con = null;
+        }
+        //System.out.println(this.getClass().getName() + " Load = " + people);
         return people;
     }
 
